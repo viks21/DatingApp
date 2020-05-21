3581a15949009de9b751f3aa5ef7630e8fd582e6
vikas dalvi <vikasdalvi2022@gmail.com>
2020-05-21 11:05:11 +0530

    section 9 complete

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { RouterModule } from '@angular/router';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { NgxGalleryModule } from 'ngx-gallery';
 
+import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,13 +14,29 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MesssagesComponent } from './messsages/messsages.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 import { apppRoutes } from './routes';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { MemberListResolver } from './_resolver/member-list.resolver';
 
 
 
+export function tokenGetter()
+{
+
+   return localStorage.getItem('token');
+}
+
+export class CustomHammerConfig extends HammerGestureConfig  {
+   overrides = {
+       pinch: { enable: false },
+       rotate: { enable: false }
+   };
+}
 
 @NgModule({
    declarations: [
@@ -28,18 +46,34 @@ import { apppRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MesssagesComponent
+      MesssagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(apppRoutes)
+      TabsModule.forRoot(),
+      NgxGalleryModule,
+      RouterModule.forRoot(apppRoutes),
+      JwtModule.forRoot({
+
+         config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains:['localhost:5000'],
+            blacklistedRoutes:['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver,
+      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+      
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { MesssagesComponent } from './messsages/messsages.component';
 import { ListsComponent } from './lists/lists.component';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { AuthGuard } from './_gaurds/auth.guard';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { MemberListResolver } from './_resolver/member-list.resolver';
 
 export const apppRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -12,7 +16,8 @@ export const apppRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MemberListComponent },
+      { path: 'members', component: MemberListComponent , resolve: {users: MemberListResolver } },
+      { path: 'members/:id', component: MemberDetailComponent , resolve: {user: MemberDetailResolver} },
       { path: 'messages', component: MesssagesComponent },
       { path: 'lists', component: ListsComponent },
     ],
